cf4087869d02ab8e5573b5fe890c6b82c397dcd6
234827 <234827@KB37-1-303-C03>
2026-09-10 17:20:31 +0300

    Добавьте файлы проекта.

---
+++ BuildAnalyzer.App/Program.cs
@@ -0,0 +1,35 @@
+﻿using BuildAnalyzer.Core;
+
+public class Program
+{
+    public static void Main()
+    {
+
+        Unit unit = new Unit();
+        Console.WriteLine("введите имя: ");
+        unit.name = Console.ReadLine();
+        Console.WriteLine("введите базовую атаку: ");
+        unit.baseAttack = double.Parse(Console.ReadLine());
+        Console.WriteLine("введите атаку оружия: ");
+        unit.weaponAttack = double.Parse(Console.ReadLine());
+        Console.WriteLine("введите шанс крит урона: ");
+        unit.critDamageChanse = int.Parse(Console.ReadLine());
+        Console.WriteLine("введите крит урон в процентах: ");
+        unit.critDamageProc = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("-------------------------------------------- ");
+        Console.WriteLine("итоговая атака: ");
+        Console.WriteLine(unit.finalAttack());
+        Console.WriteLine("сред урон: ");
+        Console.WriteLine(unit.avgDamage());
+        Console.WriteLine("шанс крит урона: ");
+        Console.WriteLine(unit.critDamageChanse);
+        Console.WriteLine("крит урон: ");
+        Console.WriteLine(unit.critDamage());
+        Console.WriteLine("рейтинг сборки: ");
+        Console.WriteLine(unit.getRate());
+    
+    
+    
+    }
+}
---
+++ BuildAnalyzer.Core/Class1.cs
@@ -0,0 +1,50 @@
+﻿namespace BuildAnalyzer.Core
+{
+
+
+    public class Unit
+    {
+        public string name {get; set;}
+        public double baseAttack {get; set;}
+        public double weaponAttack {get; set;}
+        public int critDamageChanse {get; set;}
+        public int critDamageProc {get; set;}
+
+        public double finalAttack() {
+            return baseAttack + weaponAttack;
+        }
+
+        public double avgDamage()
+        {
+            return finalAttack() * (1 + (critDamageChanse/100) * critDamage()/100);
+        }
+
+        public double critDamage()
+        {
+            return (baseAttack + weaponAttack) * (1 + (critDamageProc / 100));
+        }
+
+        public string getRate()
+        {
+            if (avgDamage() < 1500)
+            {
+                return "слабая";
+            }
+            else if (avgDamage() > 1500 && avgDamage() < 2499)
+            {
+                return "нормальная";
+            }
+            else if (avgDamage() > 2500 && avgDamage() < 3499)
+            {
+                return "хорошая";
+            }
+            else {
+                return "отличная";
+            }
+
+        }
+
+    }
+}
+
+
